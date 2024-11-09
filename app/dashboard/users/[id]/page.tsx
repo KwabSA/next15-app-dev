68864cf61beb204bb3ea8d5c284db0436a9e0fb3
KwabSA <141826847+KwabSA@users.noreply.github.com>
2024-11-09 11:35:06 +0000

    Added dynamic route

---
+++ app/dashboard/users/[id]/page.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+
+const UserProfile = ({ params }: { params: { id: string } }) => {
+  const { id } = params;
+  return (
+    <>
+      <div className="text-3xl font-bold border-b-2 mb-4">User Details</div>
+      <h1 className="text-2xl mb-8">User {id} Profile</h1>
+      <Link href={"/dashboard/users"}>Back</Link>
+    </>
+  );
+};
+
+export default UserProfile;
--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -4,8 +4,30 @@ import React from "react";
 const UsersPage = () => {
   return (
     <>
-      <div className="text-2xl">Users Page</div>
-      <Link href={"/dashboard"}>Dashboard</Link>
+      <div className="text-2xl">
+        Users Dashboard
+        <span className="text-sm">
+          &nbsp; &nbsp; &nbsp;
+          <Link href={"/dashboard"} className="underline underline-offset-4">
+            Main Dashboard
+          </Link>
+        </span>
+      </div>
+
+      <ul className="mt-8">
+        <li>
+          <Link href={"/dashboard/users/1"}>User 1</Link>
+        </li>
+        <li>
+          <Link href={"/dashboard/users/2"}>User 2</Link>
+        </li>
+        <li>
+          <Link href={"/dashboard/users/3"}>User 3</Link>
+        </li>
+        <li>
+          <Link href={"/dashboard/users/4"}>User 4</Link>
+        </li>
+      </ul>
     </>
   );
 };
