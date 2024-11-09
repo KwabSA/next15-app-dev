7982eb0ea93a6ead60e7f7cae9e485398f61c9d3
KwabSA <141826847+KwabSA@users.noreply.github.com>
2024-11-09 17:53:36 +0000

    Implemented Error Handling Routing

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,4 +1,10 @@
+import React from "react";
+
 const About = () => {
+  // const message: string;
+  throw new Error("NOT WORKING");
+  console.log("more");
+
   return <div className="text-2xl">About</div>;
 };
 
---
+++ app/(root)/error.tsx
@@ -0,0 +1,34 @@
+"use client"; // Error boundaries must be Client Components
+
+import Link from "next/link";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="mt-8">
+      <h2>Something went wrong!</h2>
+      <button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again &nbsp;
+      </button>
+      <Link href={"/"} className="underline underline-offset-4">
+        Home
+      </Link>
+    </div>
+  );
+}
